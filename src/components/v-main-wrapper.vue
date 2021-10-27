d90b9209502f772acd155670ece328951655f3e1
User <artela1992@gmail.com>
2021-10-27 13:13:44 +0300

    start app

---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="v-main-wrapper"></div>
+</template>
+
+<script>
+    export default {
+        name: 'v-main-wrapper',
+        components: {
+        },
+        prop:{},
+        data() {
+            return{
+                title: 'Hello'
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    .v-main-wrapper {
+        @apply flex justify-center items-center max-w-4xl m-auto;
+    }
+</style>
